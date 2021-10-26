f522efdf339e5a00049a14d83c22cb62e5dea240
Vikas Z <Vikas Z vikas.zingade@skillate.com>
2021-10-26 17:58:35 +0530

    create Django app cards

--- meishi/settings.py
+++ meishi/settings.py
@@ -37,7 +37,7 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
 ]
 THIRD_PARTY_APPS = ["django_extensions", "rest_framework"]
-USER_APPS = []
+USER_APPS = ["cards"]
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + USER_APPS
 
 MIDDLEWARE = [
